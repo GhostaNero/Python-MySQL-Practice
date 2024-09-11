9e89a9e9aa0f5b6917c9e8b3d3fee5e19aa85872
GhostaNero <157761388+GhostaNero@users.noreply.github.com>
2024-09-11 13:28:20 +0100

    Update mySQL&Python.py

--- python and database/mySQL&Python.py
+++ python and database/mySQL&Python.py
@@ -79,9 +79,110 @@ def codeFunction(list):
     #quit 
     print("\n")
     return 0 
-    
 
-def databaseOperation(countryCode, fileList):
+def nameFinderOperation(quickSearch, targetFirstName, targetSecondName, countryCode, fileName):
+    
+    try:
+        
+        data = []
+        
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=PASSWORD
+        )
+        #establish a cursor to interact with the database
+        mycursor = mydb.cursor()
+        #execute to use the database 
+        mycursor.execute("USE COUNTRY;")
+        mydb.commit()
+
+        quickSearchFileList = []
+        
+        for i in range(len(fileName)):
+            
+            sql = f"SELECT * FROM `{fileName[i].lower()}` WHERE firstName REGEXP '^[^abcdefghijklmnopqrstuwvzx]*$' OR secondName REGEXP  '^[^abcdefghijklmnopqrstuwvzx]*$';"
+            mycursor.execute(sql)
+            
+            data = mycursor.fetchone()
+            
+            if not data:
+                quickSearchFileList.append(fileName[i].lower())
+        
+        if countryCode:
+            
+            countryCode.lower()
+            
+            if targetFirstName:
+                
+                sql = f"SELECT * FROM `{countryCode}` WHERE firstName = `{targetFirstName}`;"
+            
+            elif targetSecondName:
+                sql = f"SELECT * FROM `{countryCode}` WHERE secondName = `{targetSecondName}`;"
+                
+            else:
+                sql = f"SELECT * FROM `{countryCode}` WHERE firstName = `{targetFirstName} AND secondName = `{targetSecondName}`;"
+            
+            mycursor.execute(sql)
+            
+            data = mycursor.fetchall()
+
+            
+
+        else:
+            
+            if quickSearch.lower() == "y":
+
+                
+                for i in range(len(quickSearchFileList)):
+                     
+                    addon = ""
+                    addon2 = ""
+                    sql = f"SELECT * FROM `{quickSearchFileList[i].lower()}` WHERE"
+                    
+                    if targetFirstName:
+                        
+                        addon = f"firstName = `{targetFirstName}`"
+                        
+                    if targetSecondName:
+                        
+                        addon2 = f""
+                    
+                    sql = sql + addon + " AND " + addon2 + ";"
+                    
+                    mycursor.execute(sql)
+                    
+                    data.append( mycursor.fetchall() )
+                
+                
+                
+            if quickSearch.lower() == 'n':
+                
+                for i in range(fileName):
+                    
+                    pass
+                    
+                    
+                
+                
+                
+                
+        if data:
+            
+            return data
+        
+        else:
+            
+            return 0
+                     
+                    
+
+    except:
+        
+        
+        print("hello")
+
+def reparimentOperation(countryCode, fileList):
     
     #establish connection to database
     mydb = mysql.connector.connect(
@@ -99,6 +200,7 @@ def databaseOperation(countryCode, fileList):
     mycursor.execute("USE COUNTRY;")
     mydb.commit()
     #if the user selected to repair the full database (which means they have to wait for so long)
+    
     if countryCode == "*":
         #loop
         for i in range(len(fileList)):
@@ -106,6 +208,9 @@ def databaseOperation(countryCode, fileList):
             name = fileList[i].replace(".csv", "")
             name = name.lower()
             #the sql and its commit
+            deleteSql = f"DROP TABLE IF EXISTS {name}"
+            mycursor.execute(deleteSql,)
+            mydb.commit()
             sql = f"CREATE TABLE IF NOT EXISTS `{name}` (firstName varchar(255), secondName varchar(255), Gender varchar(5), countryCode varchar(4) );"
             mycursor.execute(sql,)
             mydb.commit()
@@ -122,18 +227,28 @@ def databaseOperation(countryCode, fileList):
     #else if the user wants to search for a country code and repair its table
     else:
         #find the name
-        for i in range(len(fileList)):
+        
     
-            if countryCode == (name := fileList[i].replace(".csv", "").lower() ):
-                sql = f"CREATE TABLE IF NOT EXISTS `{name}` (firstName varchar(255), secondName varchar(255), Gender varchar(5), countryCode varchar(4) );"
-                mycursor.execute(sql,)
-                mydb.commit()
-                print("System log: table created for", name)
-                df = pd.read_csv(f"{ignore.path}\{fileList[i]}", header=None, dtype={0:'string', 1:'string', 2:'string', 3:'string'})
-                df.columns = ["firstName", "secondName", "Gender", "countryCode"]
-                dfUnique = df.drop_duplicates()
-                dfUnique.to_sql(name, schema='COUNTRY', con=connection, if_exists='append', index=False, chunksize=500000)
-                print("Success")
+        if countryCode in fileList:
+            
+            countryCode = countryCode.lower()
+            deleteSql = f"DROP TABLE IF EXISTS {countryCode}"
+            mycursor.execute(deleteSql,)
+            mydb.commit()
+            sql = f"CREATE TABLE IF NOT EXISTS `{countryCode}` (firstName varchar(255), secondName varchar(255), Gender varchar(5), countryCode varchar(4) );"
+            mycursor.execute(sql,)
+            mydb.commit()
+            print("System log: table created for", name)
+            df = pd.read_csv(f"{ignore.path}\{countryCode}", header=None, dtype={0:'string', 1:'string', 2:'string', 3:'string'})
+            df.columns = ["firstName", "secondName", "Gender", "countryCode"]
+            dfUnique = df.drop_duplicates()
+            dfUnique.to_sql(name, schema='COUNTRY', con=connection, if_exists='append', index=False, chunksize=500000)
+            print("Success")
+            
+        else:
+            
+            print("Error, could not find the table to repair, make sure you have typed in the right country code.")
+            return 0
                 
 
 def nameFinder(listName, listFile):
@@ -147,7 +262,15 @@ def nameFinder(listName, listFile):
         name = input("Enter (type quit to leave): ")
     
     if name != "quit":
-        print("works")
+        
+        code = input("Please enter the country code to give a more precise search (if left blank, will search through every table): ")
+        if code:
+            print("hello")
+        else:
+            print("We have detected that a country code hasn't been entered, would you like to enable quick search? (only works with english names)")
+            quickSearch = input("Please enter Y or N: ")
+        
+            
              
             
 def menuSelection():
@@ -172,15 +295,18 @@ def menuSelection():
 
 def userGuide():
     
+    #initiate the list of guide
+    listOfGuide = [tx.GENERALGUIDE, tx.COUNTRYGUIDE, tx.NAMEFINDERGUIDE, tx.USERTOOLGUIDE, tx.MANAGETOOLGUIDE, tx.REPAIRMENT]
+    #print the general text
     print(tx.USERGUIDE + tx.GUIDETEXT2 + tx.GUIDETEXT3 + tx.GUIDETEXT4)
     print("Press 7 to exit")
     userInput  = 0 
     
     while userInput != 7:
-        
+        #allow user to input a choice
         userInput = str(input("Enter: "))
         validation = False
-        
+        #validation
         while validation == False:
             
             if userInput.isdigit() == False:
@@ -194,14 +320,13 @@ def userGuide():
                  
         
         userInput = int(userInput)
-
         
-        listOfGuide = [tx.GENERALGUIDE, tx.COUNTRYGUIDE, tx.NAMEFINDERGUIDE, tx.USERTOOLGUIDE, tx.MANAGETOOLGUIDE, tx.REPAIRMENT]
         if 0 < userInput < 7:
             print(listOfGuide[userInput - 1], end="\n")
             print("To see your choice again, press 8")
         if userInput == 8:
             print(tx.GUIDETEXT4, end="\n")
+            
     return 0
     
     
@@ -210,17 +335,23 @@ def callFunction(choice, listName, listFile):
     
     if choice == 1:
         userGuide()
+        
     elif choice == 2:
         codeFunction(listName)
+        
     elif choice == 3:
         nameFinder(listName, listFile)
     
             
 def main():
+    #initiate the program by loading the csv file name
     listFile, listName = initiate()
     menuNum = 0
+    #while the user doesn't want to quit
     while menuNum != 7:
+        #allow user to select an option
         menuNum = menuSelection()
+        #use the relative function the user typed in
         callFunction(menuNum, listName, listFile)
         
     print("Thank you for using the record finder!")
